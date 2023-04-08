08aa514f9398ceffa39b560ea2e684658e7ec705
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2023-04-09 02:05:26 +0700

    finish_web

--- website/auth.py
+++ website/auth.py
@@ -1,40 +1,66 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET','POST'])
 
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form 
-    print(data)
-    return render_template('login.html', text = "Testing", user ="Note")
-
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
-
-@auth.route('/signup', methods=['GET','POST'])
-def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Please enter a valid email address', category='error')
-        elif len(firstName) < 2:
-            flash('Please enter a valid first name', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match', category='error')
-        elif len(password1) < 7: 
-            flash('Password must be at least 7 characters', category='error')
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            # add user to DB
-            new_user = User(email = email, first_name = firstName, password = password1)
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-    return render_template('signup.html')
+    return render_template("sign_up.html", user=current_user)
--- website/static/index.js
+++ website/static/index.js
@@ -3,3 +3,11 @@ document.querySelectorAll(".close").forEach(function (element) {
     this.parentElement.style.display = "none";
   });
 });
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
